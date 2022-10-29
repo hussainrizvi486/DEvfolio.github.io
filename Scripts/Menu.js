a9bc8110624f80d457ccc8b0f5d05f55ac4f1883
Hyphen <103762893+hussainrizvi486@users.noreply.github.com>
2022-10-29 21:10:03 +0500

    Update Menu.js

--- Scripts/Menu.js
+++ Scripts/Menu.js
@@ -11,17 +11,3 @@ crossIcon.addEventListener('click', ()=>{
     document.querySelector('.slider').classList.toggle('slider-go')
 } )
 
-// document.querySelector(".slider").addEventListener('click',()=>{
-//     document.querySelector(".nav").classList.toggle('go')
-//     if(document.querySelector(".nav").classList.contains('go')
-//     ){
-//         menuIcon.style.display='inline'
-//         crossIcon.style.display='none'
-//     }
-//     else{
-//         setTimeout(() => {
-//         document.querySelector("#cross").style.display='inline';}, 300)
-        
-//         document.querySelector("#menu").style.display='none'
-//     }
-// })
